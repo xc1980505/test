19fd0f63dd73c75815808a5fe2404b7d62b2171e
徐冲 <416931874@qq.com>
2020-06-18 21:48:34 +0800

    提交测试

---
+++ src/test/java/com/xc/ceshi/CeshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xc.ceshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
